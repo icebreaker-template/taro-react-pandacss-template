1152c8a08584b67c92d1c27e4d5938a50d7eef89
ice breaker <1324318532@qq.com>
2023-08-16 11:01:56 +0800

    chore: commit Pandaria's world

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "@tarojs/components";
+import { View, Text, Image } from "@tarojs/components";
 // import { useLoad } from "@tarojs/taro";
 import { css } from "styled-system/css";
 import "./index.scss";
@@ -6,15 +6,27 @@ import "./index.scss";
 const styles = css({
   bg: "yellow.200",
   rounded: "9999px",
-  fontSize: "90px",
+  fontSize: "60px",
   p: "10px 15px",
   color: "pink.500",
+  textAlign: "center",
 });
 
 export default function Index() {
   return (
-    <View className={styles}>
-      <Text>Hello world!</Text>
-    </View>
+    <>
+      <View>
+        <Image
+          className={css({
+            w: "screen",
+          })}
+          src='https://pica.zhimg.com/70/v2-6b0adc0d7cfef4698c0df39e5bb634e0_1440w'
+          mode='widthFix'
+        ></Image>
+      </View>
+      <View className={styles}>
+        <Text>Hello Pandaria's world!</Text>
+      </View>
+    </>
   );
 }
